aeaf148b476e381a55b783979c4dcfeb2082fd32
Tu Nombre <lcovarrubias@cys.com.ar>
2024-09-17 12:43:44 -0300

    se le agrega una progress bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import data_processing
+import time
 
 def main():
     st.title("App de Procesamiento de PDFs")
@@ -15,6 +16,16 @@ def main():
         if st.button("Procesar todos los archivos"):
             zip_filename = "pdfs_procesados.zip"
             data_processing.process_pdfs(uploaded_files, zip_filename)
+            progress_text = "Operation in progress. Please wait."
+            my_bar = st.progress(0, text=progress_text)
+
+            for percent_complete in range(100):
+                time.sleep(0.01)
+                my_bar.progress(percent_complete + 1, text=progress_text)
+            time.sleep(1)
+            my_bar.empty()
+
+            st.success('Procesamiento con éxito!', icon="✅")
             with open(zip_filename, "rb") as f:
                 st.download_button(
                     label="Descargar zip",
@@ -23,6 +34,8 @@ def main():
                     mime="application/zip"
                 )
             st.balloons()
+
+    st.caption("⫺ Accenture | 2024 Todos los derechos reservados ® ")
         
 if __name__ == "__main__":
     main()
